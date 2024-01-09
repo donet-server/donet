d495a916a79e7a86634798d78211f445649b9eba
maxrdz <me@maxrdz.com>
2024-01-08 23:22:47 -0700

    docs: Updated script for generating libdonet rust documentation

--- libdonet/generate-docs.sh
+++ libdonet/generate-docs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+# Meant to be ran under `libdonet-docs` branch for deployment with GH pages.
 cargo doc --no-deps
-rm -rf ./docs
+rm -rf ../docs
 echo "<meta http-equiv=\"refresh\" content=\"0; url=libdonet\">" > ../target/doc/index.html
-cp -r ../target/doc ./docs
+cp -r ../target/doc ../docs
